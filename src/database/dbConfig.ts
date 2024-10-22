eee684f77674f3de5e246d91865dc333bb1416af
sebastian2000pk <juochoas@unal.edu.co>
2024-10-22 10:22:34 -0500

    drone model

---
+++ src/database/dbConfig.ts
@@ -0,0 +1,9 @@
+export const dbConfig = {
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  tableName: "drones",
+  charset: "utf8",
+  collate: "utf8_general_ci",
+  engine: "InnoDB",
+};
---
+++ src/modules/drones/models.ts
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+import sequelize from "@/database/sequelize";
+import { dbConfig } from "@/database/dbConfig";
+
+export const Drone = sequelize.define("drone", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "ready",
+  },
+  deliveryObject: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  }
+}, dbConfig);
